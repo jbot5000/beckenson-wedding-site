200ac8f63675c25d28dff68cf7c5202ec28af7ea
Jesse Benson <hello@je553.com>
2020-10-03 13:39:37 -0500

    header image

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from "../images/ianandjesse.svg"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -16,7 +17,7 @@ const Header = ({ siteTitle }) => (
       }}
     >
       <h1 style={{ margin: 0 }}>
-        <Image src="ianandjesse.svg"></Image>
+      <img src={logo} alt="Ian and Jesse" />
         <Link
           to="/"
           style={{
