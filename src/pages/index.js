70b3a30b3183004806d1b1730ae249835a3a0515
Jesse Benson <hello@je553.com>
2020-10-06 20:37:21 -0500

    footer behaves

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,20 @@ const IndexPage = () => (
     <SEO title="Ian & Jesse's Wedding" />
     <h2>We're Getting Married!</h2>
     <p>1:00pm Friday, October 16th, 2020, in Des Moines, IA.</p>
-    <a href="#">Live Stream</a>
-    <p>Stay safe from covid-19 and join us virtually via YouTube Live.</p>
-    <a href="/page-2/">Wedding Fund</a>
-    <p>We're accepting donations to help cover the cost of our small wedding.</p>
-    <a href="tgt.gifts/beckenson">Registry</a>
-    <p>We're registered at Target. Please don't feel obligated to get us anything, though!</p>
+    <section id="links">
+      <div>
+        <a href="#">Live Stream</a>
+        <p>Stay safe from covid-19 and join us virtually via YouTube Live.</p>
+      </div>
+      <div>
+        <a href="/page-2/">Wedding Fund</a>
+        <p>We're accepting donations to help cover the cost of our small wedding.</p>
+      </div>
+      <div>
+        <a href="tgt.gifts/beckenson">Registry</a>
+        <p>We're registered at Target. Please don't feel obligated to get us anything, though!</p>
+      </div>
+    </section>
   </Layout>
 )
 
